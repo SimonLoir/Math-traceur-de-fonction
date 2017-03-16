b5a104cd2ae90ff0350c41c5e2633f2516a4bf18
SimonLoir <simon-loir@hotmail.com>
2017-03-16 21:18:38 +0100

    using vect with this.vect  instead of vect

--- Smath.js
+++ Smath.js
@@ -352,6 +352,52 @@ var SMath = function () {
         this.draw(a + "(x+" + first_exp + ")²+" + exp_2);
     }
 
+    this.vect = function (m) {
+        var x = 0;
+        var y = 0;
+        var e = m[1].replace(/\-\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)/i, function (all, x1, y1) {
+            x1 = -parseFloat(x1);
+            y1 = -parseFloat(y1);
+            return '+(' + x1 + ';' + y1 + ')';
+        })
+        
+        e = e.replace(/\-([0-9||\-||\.]+)\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)/i, function (all, multi, x1, y1) {
+            x1 = -parseFloat(x1);
+            y1 = -parseFloat(y1);
+            return '+' + multi + '(' + x1 + ';' + y1 + ')';
+        })
+
+        var vectors = e.split('+');
+
+        for (var i = 0; i < vectors.length; i++) {
+            var element = vectors[i];
+            var matchs = element.match(/^\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)$/i)
+            if (matchs != null) {
+                x += parseFloat(matchs[1]);
+                y += parseFloat(matchs[2]);
+            }
+
+            matchs = element.match(/^([0-9||\-||\.]+)\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)$/i)
+            if (matchs != null) {
+                x += parseFloat(matchs[2]) * parseFloat(matchs[1]);
+                y += parseFloat(matchs[3]) * parseFloat(matchs[1]);
+            }
+        }
+
+        if (m[2] != undefined) {
+            var x_start_vector = m[2];
+        } else {
+            var x_start_vector = 0;
+        }
+
+        if (m[3] != undefined) {
+            var y_start_vector = m[3];
+        } else {
+            var y_start_vector = 0;
+        }
+        this.newLine(x_start_vector, y_start_vector, parseFloat(x_start_vector) + x, parseFloat(y_start_vector) + y, this.color, true);
+    }
+
     this.plugin = [
         [/cos\(x\)/i, function (m) {
             this.cos();
@@ -392,10 +438,10 @@ var SMath = function () {
             }
         ],
         [
-            /^vect\[(.+)\]\[\((.+);(.+)\)\]$/i, vect
+            /^vect\[(.+)\]\[\((.+);(.+)\)\]$/i, this.vect
         ],
         [
-            /^vect\[(.+)\]$/i, vect
+            /^vect\[(.+)\]$/i, this.vect
         ]
     ];
     this.execPlugin = function (val) {
@@ -446,49 +492,6 @@ var SMath = function () {
             start += 0.001;
         }
     }
+
 }
 
-function vect(m) {
-                var x = 0;
-                var y = 0;
-                var e = m[1].replace(/\-\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)/i, function (all, x1, y1) {
-                    x1 = -parseFloat(x1);
-                    y1 = -parseFloat(y1);
-                    return '+(' + x1 + ';' + y1 + ')'
-                })
-                e = e.replace(/\-([0-9||\-||\.]+)\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)/i, function (all, multi, x1, y1) {
-                    x1 = -parseFloat(x1);
-                    y1 = -parseFloat(y1);
-                    return '+' + multi + '(' + x1 + ';' + y1 + ')'
-                })
-                var vectors = e.split('+');
-
-                for (var i = 0; i < vectors.length; i++) {
-                    var element = vectors[i];
-                    var matchs = element.match(/^\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)$/i)
-                    if (matchs != null) {
-                        x += parseFloat(matchs[1]);
-                        y += parseFloat(matchs[2]);
-                    }
-
-                    matchs = element.match(/^([0-9||\-||\.]+)\(([0-9||\-||\.]+);([0-9||\-||\.]+)\)$/i)
-                    if (matchs != null) {
-                        x += parseFloat(matchs[2]) * parseFloat(matchs[1]);
-                        y += parseFloat(matchs[3]) * parseFloat(matchs[1]);
-                    }
-                }
-
-                if(m[2] != undefined){
-                    var x_start_vector = m[2];
-                }else{
-                    var x_start_vector = 0;
-                }
-
-                if(m[3] != undefined){
-                    var y_start_vector = m[3];
-                }else{
-                    var y_start_vector = 0;
-                }
-                alert(x + "=> " + y)
-                this.newLine(x_start_vector, y_start_vector, parseFloat(x_start_vector) + x, parseFloat(y_start_vector) +  y, this.color, true);
-            }
