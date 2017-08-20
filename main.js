797577e561dde88effce5258769d8fcd49cb6a28
SimonLoir <simon-loir@hotmail.com>
2017-08-20 10:42:20 +0200

    adding launch screen for teh desktop app

--- main.js
+++ main.js
@@ -9,15 +9,31 @@ const url = require('url');
 
 app.on("ready", function () {
     const screen = electron.screen;
-    var main_window = new bw();
-    main_window.maximize();
-    main_window.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
+
+    var welcome =  new bw({frame:false});
+
+    welcome.loadURL(url.format({
+        pathname: path.join(__dirname, "launch.html"),
         protocol: "file:",
         slashes: true
     }));
-    main_window.on("closed", function () {
-        main_window = null;
-        app.exit();
-    }); 
+
+    setTimeout(function () {
+        var main_window = new bw({show:false});
+        welcome.close();
+        
+        main_window.maximize();
+        main_window.loadURL(url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true
+        }));
+        main_window.on("closed", function () {
+            main_window = null;
+            app.exit();
+        }); 
+        setTimeout(function() {
+            main_window.show();
+        }, 500);
+    }, 2500);
 });
