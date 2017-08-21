ccbb26bf4667fd1fbaba7fd5ec526b6faef53d97
SimonLoir <simon-loir@hotmail.com>
2017-08-21 21:56:49 +0200

    basic support for stats

--- stats/stats.js
+++ stats/stats.js
@@ -22,31 +22,61 @@ $(document).ready(function () {
 
 
     $("#new").click(function () {
+        var use_classes = $('#use_classes').node.checked;
 
-        prompt('xi (caractère)', "", function (x) {
-            if (x == null || x.trim() == "") {
-                return;
+        if(use_classes){
+
+            if(s.stats.buffer.brackets == undefined){
+                prompt('Lors de la définition d\' une classe, nous utilisons la nomenclature suivante : [nbr;nbr[ ou ]nbr;nbr]. Utilisez-vous ] ou [ ? ', "[", function(x) {
+                    s.stats.buffer.brackets = x;
+                    asker_continue(x, true, chart);
+                });
+            }else{
+                asker_continue(s.stats.buffer.brackets, true, chart);                
             }
-            prompt('Effectif', 1, function (e) {
 
-                if (e == null || e.trim() == "") {
-                    return;
-                }
-                e = parseFloat(e);
-                chart.setDataSets([]);
-                if (s.stats.buffer.tab[x.trim()] != undefined) {
-                    s.stats.buffer.tab[x.trim()] += e;
-                } else {
-                    s.stats.buffer.tab[x.trim()] = e;
-                }
+        }else{
+            asker_continue("", false, chart);
+        }
 
-                build();
-            });
-        });
+        
 
     });
 
 });
+
+
+function asker_continue(response, use_classes, chart){
+    if(use_classes == true){
+        if(response.trim() != "[" && response.trim() != "]"){
+            alert('Erreur : argument invalide');
+            return false;
+        }
+        text1 = "Classe (sans les [ ou ] ). Notez que la classe est définie en séparant les valeurs par un ; et non une ,";
+    }else{
+        text1 = "xi";
+    }
+    prompt(text1, "", function (x) {
+        if (x == null || x.trim() == "") {
+            return;
+        }
+        prompt('Effectif', 1, function (e) {
+
+            if (e == null || e.trim() == "") {
+                return;
+            }
+            e = parseFloat(e);
+            chart.setDataSets([]);
+            if (s.stats.buffer.tab[x.trim()] != undefined) {
+                s.stats.buffer.tab[x.trim()] += e;
+            } else {
+                s.stats.buffer.tab[x.trim()] = e;
+            }
+
+            build(use_classes, chart);
+        });
+    });
+}
 
 window.prompt = function (text, value, callback) {
     var id = "#prompt_dialog";
@@ -60,9 +90,6 @@ window.prompt = function (text, value, callback) {
     if (value != undefined) {
         document.querySelector("#prompt_default").value = value;
     }
-
-    //pvalidate
-    //pcancel
 
     var validate = document.querySelector("#pvalidate");
     var val = "";
@@ -83,14 +110,14 @@ function sleep(ms) {
     while (new Date() < ms) { }
 }
 
-function build() {
+function build(use_classes, chart) {
     var keys = Object.keys(s.stats.buffer.tab);
 
     keys = keys.sort();
 
     var table = document.querySelector('.e_table');
 
-    var use_classes = $('#use_classes').node.checked;
+    
 
     if (use_classes == true) {
         var th0 = "<th>Classe</th>";
@@ -102,9 +129,24 @@ function build() {
     var eff = 0;
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
+        if(use_classes == true){
+            var td0 = "<td>" + s.stats.buffer.brackets + key +  s.stats.buffer.brackets + "</td>";
+            key = key.replace('[', "").replace(']', "");
+            if(key.indexOf(';') <= 0){
+                text = key + ":" + key;
+            }else{
+                text = key;
+            }
+            var td1 = ""
+        }else{
+            var td0 = "";
+            var td1 = key;
+            text = key;      
+        }
         eff += s.stats.buffer.tab[key];
         table.innerHTML += "<tr>"
-            + "<td>" + key + "</td>"
+            + td0
+            + "<td>" + td1 + "</td>"
             + "<td>" + s.stats.buffer.tab[key] + "</td>"
             + "<td>" + eff + "</td>"
             + "</tr>";
