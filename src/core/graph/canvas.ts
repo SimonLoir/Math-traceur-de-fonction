973015f53f927b3dce6f072209943e2e683050ed
SimonLoir <simon-loir@hotmail.com>
2019-02-03 12:18:26 +0100

    [graphing] added a working grid

--- src/core/graph/canvas.ts
+++ src/core/graph/canvas.ts
@@ -32,4 +32,62 @@ export default class canvas {
         this.ctx.fill();
         this.ctx.closePath();
     }
+
+    /**
+     * Draws a line in the canvas
+     * @param x The x of the starting point
+     * @param y The y of the starting point
+     * @param x2 The x of the ending point
+     * @param y2 The  of the ending point
+     * @param color (?) The color of the line
+     * @param width (?) The thickness of the line
+     */
+    public drawLine(
+        x: number,
+        y: number,
+        x2: number,
+        y2: number,
+        color?: string,
+        width?: number
+    ) {
+        this.ctx.beginPath();
+
+        if (color == undefined) {
+            this.ctx.strokeStyle = '#D0D0D0';
+        } else {
+            this.ctx.strokeStyle = color;
+        }
+
+        if (y2 == Infinity) console.log((y2 = this.canvas.height), 'Inf' + y2);
+        if (y == Infinity) console.log((y = this.canvas.height), 'Inf' + y);
+
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x2, y2);
+        if (width == undefined) {
+            this.ctx.lineWidth = 1;
+        } else {
+            this.ctx.lineWidth = width;
+        }
+        this.ctx.stroke();
+    }
+
+    public text(
+        text: string,
+        x: number,
+        y: number,
+        color = 'gray',
+        font = '18px Sans Serif'
+    ) {
+        this.ctx.beginPath();
+        this.ctx.font = font;
+        this.ctx.fillStyle = color;
+        this.ctx.fillText(text, x, y);
+    }
+
+    public get height() {
+        return this.canvas.height;
+    }
+    public get width() {
+        return this.canvas.width;
+    }
 }
--- src/core/graph/graph.ts
+++ src/core/graph/graph.ts
@@ -11,7 +11,11 @@ export default class graph {
     /**
      * The size in px of one unit
      */
-    public scale: number = 50;
+    public x_scale: number = 50;
+    /**
+     * The size in px of one unit
+     */
+    public y_scale: number = 50;
     /**
      * The unit on the x axis
      */
@@ -23,11 +27,11 @@ export default class graph {
     /**
      * The x value on the center of the graph
      */
-    public x_center: number = 0;
+    public x_center: number = 1;
     /**
      * The y value on the center of the graph
      */
-    public y_center: number = 0;
+    public y_center: number = 1;
     private objects: graphObject[] = [];
     constructor(public canvas: canvas) {
         this.objects.push(new Point(1, 0));
@@ -36,5 +40,106 @@ export default class graph {
     public register(type: string, opts: any) {}
     public draw() {
         this.canvas.clear();
+        this.drawGrid();
+    }
+    /**
+     * Creates a grid
+     */
+    public drawGrid() {
+        let size = 18;
+        let phase = 15;
+        let font = size + 'px Sans Serif';
+        let xMax = this.canvas.width / this.x_scale / 2 + this.x_unit;
+        let start = this.x_center - (this.x_center % this.x_unit);
+        let y0 = this.yPos(0);
+        for (let x = 0; x < xMax; x += this.x_unit) {
+            let xpos = this.xPos(start + x);
+            let xposMinus = this.xPos(start - x);
+            this.canvas.drawLine(xpos, 0, xpos, this.canvas.height);
+            this.canvas.text(
+                (Math.floor((start + x) * 100) / 100).toString(),
+                xpos,
+                y0 + phase,
+                'gray',
+                font
+            );
+            if (xpos != xposMinus) {
+                this.canvas.drawLine(
+                    xposMinus,
+                    0,
+                    xposMinus,
+                    this.canvas.height
+                );
+                this.canvas.text(
+                    (Math.floor((start - x) * 100) / 100).toString(),
+                    xposMinus,
+                    y0 + phase,
+                    'gray',
+                    font
+                );
+            }
+        }
+        let yMax = this.canvas.height / this.y_scale / 2 + this.y_unit;
+        let yStart = this.y_center - (this.y_center % this.y_unit);
+        let x0 = this.xPos(0);
+        for (let y = 0; y < yMax; y += this.y_unit) {
+            let yPlus = yStart + y;
+            let yMinus = yStart - y;
+            let ypos = this.yPos(yPlus);
+            let yposMinus = this.yPos(yMinus);
+            this.canvas.drawLine(0, ypos, this.canvas.width, ypos);
+            this.canvas.text(
+                (Math.floor(yPlus * 100) / 100).toString(),
+                x0 - phase,
+                ypos,
+                'gray',
+                font
+            );
+            if (ypos != yposMinus) {
+                this.canvas.drawLine(
+                    0,
+                    yposMinus,
+                    this.canvas.width,
+                    yposMinus
+                );
+                this.canvas.text(
+                    (Math.floor(yMinus * 100) / 100).toString(),
+                    x0 - phase,
+                    yposMinus,
+                    'gray',
+                    font
+                );
+            }
+        }
+        this.canvas.drawLine(
+            this.xPos(0),
+            0,
+            this.xPos(0),
+            this.canvas.height,
+            'black'
+        );
+        this.canvas.drawLine(
+            0,
+            this.yPos(0),
+            this.canvas.width,
+            this.yPos(0),
+            'black'
+        );
+    }
+
+    private xPos(point: number) {
+        return (
+            this.canvas.width / 2 +
+            point * this.x_scale -
+            this.x_center * this.x_scale
+        );
+    }
+
+    private yPos(point: number) {
+        return (
+            this.canvas.height / 2 -
+            point * this.y_scale +
+            this.y_center * this.y_scale
+        );
     }
 }
