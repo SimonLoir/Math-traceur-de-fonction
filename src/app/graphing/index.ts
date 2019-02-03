036e860d046744323ac9b5299c22974b4b6914da
SimonLoir <simon-loir@hotmail.com>
2019-02-03 12:31:06 +0100

    spaced

--- src/app/graphing/index.ts
+++ src/app/graphing/index.ts
@@ -2,9 +2,12 @@ import smath from '../../core';
 import '../style/graphing.scss';
 import canvas from '../../core/graph/canvas';
 import { $ } from '../../extjs';
+
 let s = new smath();
+
 let g = s.graph.create(new canvas(document.querySelector('canvas')));
 g.draw();
+
 $('#objectButton').click(() => {
     let tk = $('#toolkit');
     if (tk.hasClass('hidden')) tk.removeClass('hidden');
